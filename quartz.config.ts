6ceb9aca4aef15aaf2b4c5eaa6e4eab7b3a763d4
cyoltose <bilibilicookie@outlook.com>
2026-01-07 21:56:46 +0800

    11

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "cyoltose509.github.io/my-anime-list",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
